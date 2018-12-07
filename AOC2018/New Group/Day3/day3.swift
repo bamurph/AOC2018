1b9f57d5163df706a42833fdca8671e58db55e94
Ben Murphy <badesignco@icloud.com>
2018-12-07 06:38:05 -0800

    ACTUAL right answer for 3

--- AOC2018/New Group/Day3/day3.swift
+++ AOC2018/New Group/Day3/day3.swift
@@ -8,22 +8,23 @@
 
 import Foundation
 
-
+let input3e = """
+#1 @ 1,3: 4x4
+#2 @ 3,1: 4x4
+#3 @ 5,5: 2x2
+"""
 
 func dayThree() {
 
     let splitInput = input3
         .components(separatedBy: .newlines)
         .map {
-            $0.replacingOccurrences(of: "@ ", with: "")
-                .replacingOccurrences(of: "#", with: "")
-                .replacingOccurrences(of: ",", with: " ")
-                .replacingOccurrences(of: ":", with: "")
-                .replacingOccurrences(of: "x", with: " ")
-                .components(separatedBy: .whitespaces)
+            $0.replacingOccurrences(of: " @ ", with: ",")
+                .replacingOccurrences(of: ": ", with: ",")
+                .replacingOccurrences(of: "x", with: ",")
+                .components(separatedBy: ",")
                 .map { Int($0) }}
 
-    let a = splitInput[4]
     var fabric = Array(repeating: (Array(repeating: 0, count: 2500)), count: 2500)
     for area in splitInput {
         let xPos = area[1]
@@ -39,44 +40,46 @@ func dayThree() {
     }
 
     let flatFabric = fabric
-        .map { $0.reduce(0, { $1 > 1  ? $0 + 1 : $0 }) }
-        .reduce(0, { $1 > 1 ? $0 + 1 : $0 })
+        .map { $0.filter { a in a > 1 }.count }
+        .reduce(0, +)
+
+
     print(flatFabric)
 
-// Used a known good result to find my mistake. OFF BY ONE!
-//    func harshilsCode() -> String{
-//        let lines = input3
-//            .trimmingCharacters(in: .whitespaces)
-//            .components(separatedBy: .newlines)
-//
-//        var board = Array(repeating: Array(repeating: 0, count: 2000), count: 2000)
-//
-//        lines.forEach { line in
-//            let components = line.components(separatedBy: .whitespaces)
-//            let insets = components[2]
-//            let xInset = Int(insets.components(separatedBy: .punctuationCharacters)[0])!
-//            let yInset = Int(insets.components(separatedBy: .punctuationCharacters)[1])!
-//
-//            let size = components[3]
-//            let xSize = Int(size.prefix(while: { $0 != "x" }))!
-//            let ySize = Int(size.drop(while: { $0 != "x" }).dropFirst())!
-//
-//            (xInset ..< xInset + xSize).forEach { x in
-//                (yInset ..< yInset + ySize).forEach { y in
-//                    board[x][y] += 1
-//                }
-//            }
-//        }
-//
-//        func partOne() -> String {
-//            let overlappingCount = board
-//                .map { $0.reduce(0, { $1 > 1  ? $0 + 1 : $0 }) }
-//                .reduce(0, { $1 > 1 ? $0 + 1 : $0 })
-//
-//            return "\(overlappingCount)"
-//        }
-//        return partOne()
-//    }
+ //Used a known good result to find my mistake. OFF BY ONE!
+    func harshilsCode() -> String{
+        let lines = input3
+            .trimmingCharacters(in: .whitespaces)
+            .components(separatedBy: .newlines)
+
+        var board = Array(repeating: Array(repeating: 0, count: 2000), count: 2000)
+
+        lines.forEach { line in
+            let components = line.components(separatedBy: .whitespaces)
+            let insets = components[2]
+            let xInset = Int(insets.components(separatedBy: .punctuationCharacters)[0])!
+            let yInset = Int(insets.components(separatedBy: .punctuationCharacters)[1])!
+
+            let size = components[3]
+            let xSize = Int(size.prefix(while: { $0 != "x" }))!
+            let ySize = Int(size.drop(while: { $0 != "x" }).dropFirst())!
+
+            (xInset ..< xInset + xSize).forEach { x in
+                (yInset ..< yInset + ySize).forEach { y in
+                    board[x][y] += 1
+                }
+            }
+        }
+
+        func partOne() -> String {
+            let overlappingCount = board
+                .map { $0.reduce(0, { $1 > 1  ? $0 + 1 : $0 }) }
+                .reduce(0, { $1 > 1 ? $0 + 1 : $0 })
+
+            return "\(overlappingCount)"
+        }
+        return partOne()
+    }
 
     print(harshilsCode())
 }
