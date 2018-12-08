7a88159417ce41ebf73bf8eb13540858aab3483a
Ben Murphy <badesignco@icloud.com>
2018-12-08 07:33:13 -0800

    Set up day 4 + input

---
+++ AOC2018/Day4/day4.swift
@@ -0,0 +1,13 @@
+//
+//  day4.swift
+//  AOC2018
+//
+//  Created by Ben Murphy on 12/8/18.
+//  Copyright © 2018 Constellation Software. All rights reserved.
+//
+
+import Foundation
+
+func dayFour() {
+
+}
