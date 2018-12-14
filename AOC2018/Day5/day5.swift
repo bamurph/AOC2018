ea8e123d8dfe500f39662dda8ff29a5a4fe28af2
Ben Murphy <badesignco@icloud.com>
2018-12-14 09:12:58 -0800

    5.1 done

--- AOC2018/Day5/day5.swift
+++ AOC2018/Day5/day5.swift
@@ -12,11 +12,31 @@ extension Character {
     func isUpperCase() -> Bool {
         return CharacterSet.uppercaseLetters.contains(self.unicodeScalars.first!)
     }
+
+    func isLowerCase() -> Bool {
+        return !self.isUpperCase()
+    }
+
+    func isOpposite(of other: Character) -> Bool {
+        if String(self).uppercased() == String(other).uppercased() {
+            if self.isUpperCase() && other.isLowerCase() { return true }
+            if self.isLowerCase() && other.isUpperCase() { return true }
+        }
+       return false
+    }
 }
 
 func dayFive() {
+    // Part 1
+    let polymer = input5
 
-    let polymer = sample5
+    let reduced = polymer
+        .reduce(into: "") { acc, next in
+            if acc.count == 0 { acc.append(next); return }
+            if acc.last!.isOpposite(of: next) { acc.removeLast(); return }
+            acc.append(next)
+    }
+    print(reduced.count)
 
-
+    // Part 2
 }
