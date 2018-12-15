4dbd75ef75857828bb022e4f20776f5b34d9027d
Ben Murphy <badesignco@icloud.com>
2018-12-14 17:43:43 -0800

    Day six find finite coords

--- AOC2018/Day6/day6.swift
+++ AOC2018/Day6/day6.swift
@@ -8,13 +8,40 @@
 
 import Foundation
 
+fileprivate func distanceBetween(first: Coordinate, second: Coordinate) -> Int {
+    let xDistance = abs(first.x - second.x)
+    let yDistance = abs(first.y - second.y)
+    return (xDistance + yDistance)
+}
+
+fileprivate struct Coordinate {
+    let x: Int
+    let y: Int
+
+    func distanceTo(_ second: Coordinate) -> Int {
+        return distanceBetween(first: self, second: second)
+    }
+
+    func isBounded(among: [Coordinate]) -> Bool {
+        let northwest = among.contains { $0.x < self.x && $0.y < self.y }
+        let northeast = among.contains { $0.x > self.x && $0.y < self.y }
+        let southwest = among.contains { $0.x < self.x && $0.y > self.y }
+        let southeast = among.contains { $0.x > self.x && $0.y > self.y }
+        return (northeast && northwest && southwest && southeast)
+    }
+
+}
+
 func daySix() {
 
     let lines = sample6.components(separatedBy: .newlines)
 
-    let coordinates = lines.map { line -> (Int, Int) in
+    let coordinates = lines.map { line -> Coordinate in
         let coords = line.components(separatedBy: ", ")
-        return (Int(coords[0])!, Int(coords[1])!)
+        return Coordinate(x: Int(coords[0])!, y: Int(coords[1])!)
     }
-    
+
+    let finiteCoordinates = coordinates.filter { $0.isBounded(among: coordinates)}
+
+
 }
