91969f07b646ca5884b88e85a3be979c8a30658b
Ben Murphy <badesignco@icloud.com>
2018-12-15 09:59:27 -0800

    Finish 7.1

--- AOC2018/Day7/day7.swift
+++ AOC2018/Day7/day7.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 // Parse out steps with predicates like "step: [predicate]"
-fileprivate let steps = sample7
+fileprivate let steps = input7
     .components(separatedBy: .newlines)
     .map { $0.components(separatedBy: " ") }
     .map { [$0[7]: $0[1]] }
@@ -22,31 +22,44 @@ let all = afters.union(befores).map {String($0)}
 
 func daySeven() {
 
-   let a = all.sorted()
+
     // Determine the order in which steps should be completed, if more than one is ready then do alphabetically.
 
     // Start by finding first step that has no unsatisfied predicates
 
-    var completed = all
-        .filter { befores.contains($0.first!) != true }
+    let fs = all.filter { befores.contains($0.first!) != true }
         .sorted()
-        .first!
+        .joined()
 
-    func workThrough(list: [String: String], completed: String) -> String {
-        let completable = list.filter { completed
-            .contains($0.value) == true }
-            .min { a, b in a.key < b.key }
-        if completable == nil { return completed }
-        let newlist = list.filter { $0 != completable!}
-        let updatedCompleted = completed + completable!.key
 
-        let toReturn = workThrough(list: newlist, completed: updatedCompleted)
+
+
+
+    func sortThrough(list: [String: String], completed: String, initials: String) -> String {
+
+
+
+        let completable = list
+            .filter { Set($0.value).isSubset(of: Set(completed))}
+            .map { $0.key }
+            .sorted().joined() + initials
+
+        if completable == "" { return completed }
+
+        let next = String(completable.first!)
+
+        let newInitials = initials.replacingOccurrences(of: next, with: "")
+
+        let newlist = list.filter { $0.key != next}
+        let updatedCompleted = completed + next
+
+        let toReturn = sortThrough(list: newlist, completed: updatedCompleted, initials: newInitials)
         return toReturn
     }
 
-    print(workThrough(list: steps, completed: completed))
+    let ls = sortThrough(list: steps, completed: "", initials: String(fs))
 
 
-
+    print(ls)
 
 }
